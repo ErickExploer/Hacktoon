90fec12e1a2d4d4b01f4a6fa73267c3f02e164b3
ErickExploer <152933429+ErickExploer@users.noreply.github.com>
2024-05-21 14:09:12 -0500

    Add files via upload

---
+++ Hacktoon/src/main/java/com/example/hacktoon/HacktoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hacktoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacktoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HacktoonApplication.class, args);
+    }
+
+}
---
+++ Hacktoon/src/test/java/com/example/hacktoon/HacktoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hacktoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacktoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
